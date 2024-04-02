87ea036d1b16673660c5bfe202acdceb93c59fb7
Dimpleg286 <153986330+Dimpleg286@users.noreply.github.com>
2024-04-02 13:11:32 -0500

    Day1

---
+++ src/BasicOfJava/Datatypes.java
@@ -0,0 +1,22 @@
+package BasicOfJava;
+
+public class Datatypes {
+/*identifiers:they are used to name in java program is know as identifiers
+ which can be used for identification it can be class name, method name,
+ variable name, lable name */
+	
+/*java comments:Comments improve the readability of the code and makes the 
+code more clear and understandable. Comments are ignored by the Compiler. 
+singleline //,multiline/*--*/   //  document/**---*/ 
+
+/*keywords:they are also called reserved words they have special meaning in 
+ programming language and they cannot be used as identifiers */
+
+/*recognize Datatype
+ * nonprementive datatype A reference variable is used to refer to any object 
+of the declared type or the compatible type. 
+Example: String s=new String("balloon"); */
+	
+//applyCasting and conversion
+	
+}
